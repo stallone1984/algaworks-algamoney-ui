ded08523b5c299d91ce27fd199639952356ce670
Heitor Bernardino <heitor.albano1984@gmail.com>
2020-02-23 20:15:12 -0300

    17-16 Desafio: implementando o serviço de listagem de categorias

---
+++ src/app/categorias/categoria.dto.ts
@@ -0,0 +1,4 @@
+export interface CategoriaDTO {
+  codigo: number;
+  nome: string;
+}
---
+++ src/app/categorias/categoria.service.ts
@@ -0,0 +1,20 @@
+import { CategoriaDTO } from './categoria.dto';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriaService {
+
+  urlCategorias = 'http://localhost:8080/categorias';
+
+  constructor(private http: HttpClient) { }
+
+  listarTodas(): Promise<CategoriaDTO> {
+    return this.http.get<CategoriaDTO>(this.urlCategorias)
+    .toPromise()
+    .then(response => response);
+  }
+
+}
